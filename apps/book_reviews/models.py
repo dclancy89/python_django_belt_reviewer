06b8bbea5820d45bee6bfd16a8d8ddbd9b269561
Daniel Clancy <danielclancy@Daniels-MBP.hsd1.il.comcast.net>
2018-03-12 13:57:34 -0500

    added user model and validation

--- apps/book_reviews/models.py
+++ apps/book_reviews/models.py
@@ -1,5 +1,54 @@
 from __future__ import unicode_literals
 
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Manager):
+	def validate_user(request, postData):
+		errors = {}
+
+
+		# validate Name
+		if len(postData['name']) < 2 or not postData['name'].isalpha():
+			if len(postData['name']) < 2:
+				errors['name_length'] = "Name must be at least two characters."
+			if not postData['first_name'].isalpha():
+				errors['name_alpha'] = "Name can only contain letters."
+
+	
+
+		# validate email
+		try:
+			validate_email(postData['email'])
+		except ValidationError:
+			errors['email'] = "This is not a valid email."
+		else:
+
+			if User.objects.filter(email=postData['email']):
+				errors['email'] = "This user already exists."
+
+
+		# validate password
+		if len(postData['password']) < 8:
+			if len(postData['password']) < 8:
+				errors['password_length'] = "Password must be at least 8 characters."
+
+		# check if passwords match
+		if postData['password'] != postData['confirm_pw']:
+			errors['confirm_pw'] = "Passwords must match"
+
+
+		return errors
+
+class User(models.Model):
+	name = models.CharField(max_length=255)
+	alias = models.CharField(max_length=255)
+	email = models.CharField(max_length=255)
+	password = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add = True)
+	updated_at = models.DateTimeField(auto_now = True)
+
+	objects = UserManager()
--- apps/book_reviews/views.py
+++ apps/book_reviews/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, HttpResponse, redirect
+import bcrypt
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from django.contrib import messages
+from .models import *
 
 # Create your views here.
 def index(request):
 	return render(request, 'book_reviews/index.html')
 
 def register(request):
+	errors = User.objects.validate_user(request.POST)
+
+	if len(errors):
+		for tag, error in errors.iteritems():
+			messages.error(request, error)
+		return redirect('/')
+
+
 	return redirect('/')
 
 def login(request):
